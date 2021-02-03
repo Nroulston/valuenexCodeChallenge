2edd1a6c6c7e28ac36e872d00739c671f8de133f
Nroulston <nicolas.roulston@gmail.com>
2021-02-03 11:15:01 -0700

    create CSV Importer component, render using react-dom-router

---
+++ frontend/src/features/CSVImporter.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+import { CSVReader } from 'react-papaparse'
+
+export default class CSVImporter extends Component {
+  handleOnDrop = (data) => {
+    console.log('---------------------------')
+    console.log(data)
+    console.log('---------------------------')
+  }
+
+  handleOnError = (err, file, inputElem, reason) => {
+    console.log(err)
+  }
+
+  handleOnRemoveFile = (data) => {
+    console.log('---------------------------')
+    console.log(data)
+    console.log('---------------------------')
+  }
+
+  render() {
+    return (
+      <CSVReader
+        onDrop={this.handleOnDrop}
+        onError={this.handleOnError}
+        addRemoveButton
+        onRemoveFile={this.handleOnRemoveFile}
+      >
+        <span>Drop CSV file here or click to upload.</span>
+      </CSVReader>
+    )
+  }
+}
