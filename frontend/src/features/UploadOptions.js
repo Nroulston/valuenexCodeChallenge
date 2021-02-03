85b2439a2822e35da8cf7123e79a1033d17189b3
Nroulston <nicolas.roulston@gmail.com>
2021-02-03 16:18:07 -0700

    create dynamically generated select boxes

--- frontend/src/features/UploadOptions.js
+++ frontend/src/features/UploadOptions.js
@@ -12,34 +12,76 @@ export const UploadOptions = (props) => {
   }
 
   const [checkedItems, setCheckedItems] = useState(options)
+  const [selectID, setSelectID] = useState('')
+  const [selectName, setSelectName] = useState('')
+  const [selectTimeStamp, setSelectTimeStamp] = useState('')
   
-  const handleChange = (event) => {
-   
+  const handleCheckedChange = (event) => {
     setCheckedItems({...checkedItems, [event.target.name] : event.target.checked})
-    
-}
+  }
+
+  const handleSelectChange = (event) => {
+    switch (event.target.name) {
+      case "ID":
+        setSelectID(event.target.value);
+        break;
+      case "Name":
+        setSelectName(event.target.value)
+        break;
+      case "TimeStamp":
+        setSelectTimeStamp(event.target.value)
+        break;
+    }
+  }
 
   const inputCheckBoxes = props.CSVData[0].data.map((option, index) => {
-
     return (
     <>
-      
-      <label>{option} </label> 
-      <input
-        style={inputStyle}
-        key={index}
-        name={option}
-        type="checkbox"
-        checked={checkedItems[option]}
-        onChange={handleChange} />
-     </>
+      <label key={index} >
+        {option}
+        <input
+          style={inputStyle}
+          name={option}
+          type="checkbox"
+          checked={checkedItems[option]}
+          onChange={handleCheckedChange} 
+        />
+      </label>   
+    </>
   )})
 
+  const inputSelectBoxOptions = props.CSVData[0].data.map((option, index) => {
+    return (  
+      <option value={option}>{option}</option>
+    )
+  })
+
+  const inputSelectBoxes = [
+    <label>
+        ID
+        <select value={selectID} name="ID" onChange={handleSelectChange}>
+          {inputSelectBoxOptions} 
+        </select>  
+    </label>,
+    <label>
+        Name
+        <select value={selectName} name="Name" onChange={handleSelectChange}>
+          {inputSelectBoxOptions} 
+        </select>  
+    </label>,
+    <label>
+        TimeStamp
+        <select value={selectTimeStamp} name="TimeStamp" onChange={handleSelectChange}>
+          {inputSelectBoxOptions} 
+        </select>  
+    </label>
+  ]
   return(
     <>
     <p>Exclude Columns by untoggling the checkbox</p>
     <form>
       { inputCheckBoxes }
+      { inputSelectBoxes}
     </form>
     
     </>
